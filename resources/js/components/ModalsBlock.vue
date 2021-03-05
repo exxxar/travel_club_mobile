797fa9de8935c03b8606009bcd4542a0008a1681
unknown <exxxar@gmail.com>
2021-03-05 23:29:40 +0300

    seo-правки

--- resources/js/components/ModalsBlock.vue
+++ resources/js/components/ModalsBlock.vue
@@ -3,17 +3,19 @@
         <modal-about/>
         <modal-contact/>
         <modal-rules/>
+        <modal-add-to-home/>
     </div>
 </template>
 <script>
     import ModalAbout from './Modals/ModalAbout'
     import ModalContact from './Modals/ModalContact'
     import ModalRules from './Modals/ModalRules'
+    import ModalAddToHome from './Modals/ModalAddToHome'
 
 
     export default {
         components: {
-            ModalAbout, ModalContact, ModalRules
+            ModalAbout, ModalContact, ModalRules, ModalAddToHome
         }
     }
 </script>
