e25a09ea5455c8152b04e638a5d068bdc1d4b1f2
unknown <exxxar@gmail.com>
2021-03-06 00:01:32 +0300

    update pwa

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,6 +19,10 @@ Vue.use(Tawk, {
 })
 
 
+import VuePwaInstallPlugin from "vue-pwa-install";
+
+Vue.use(VuePwaInstallPlugin);
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
