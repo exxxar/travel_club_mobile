3b2692bbabc48c73b28b634bdd8be899666e5452
unknown <exxxar@gmail.com>
2021-03-11 10:26:22 +0300

    set maintenance page if offline

--- routes/web.php
+++ routes/web.php
@@ -21,24 +21,24 @@ Route::get('/', function () {
     return view('welcome');
 })->name("desktop");
 
-Route::get('/offline', function (){
-    return "offline page";
+Route::get('/offline', function () {
+    return view("maintenance");
 })->name("offline");
 
 
 Route::view('/m/', 'pages.index')->name("index");
 
-Route::view('/m/about',"pages.about")->name("about");
-Route::view('/m/contact',"pages.contact")->name("contact");
-Route::view('/m/chat',"pages.chat")->name("chat");
-Route::view('/m/faq',"pages.faq")->name("faq");
+Route::view('/m/about', "pages.about")->name("about");
+Route::view('/m/contact', "pages.contact")->name("contact");
+Route::view('/m/chat', "pages.chat")->name("chat");
+Route::view('/m/faq', "pages.faq")->name("faq");
 
-Route::view('/m/tours',"pages.tours")->name("tours");
-Route::view('/m/profile',"pages.profile")->name("profile");
-Route::view('/m/adventure',"pages.adventure")->name("adventure");
-Route::view('/m/hotels',"pages.hotels")->name("hotels");
-Route::view('/m/flies',"pages.flies")->name("flies");
-Route::view('/m/promo',"pages.promo")->name("promo");
-Route::view('/m/maintenance',"pages.maintenance")->name("maintenance");
-Route::view('/m/tour-search',"pages.tour-search")->name("tour-search");
-Route::view('/m/avia-search',"pages.avia-search")->name("avia-search");
+Route::view('/m/tours', "pages.tours")->name("tours");
+Route::view('/m/profile', "pages.profile")->name("profile");
+Route::view('/m/adventure', "pages.adventure")->name("adventure");
+Route::view('/m/hotels', "pages.hotels")->name("hotels");
+Route::view('/m/flies', "pages.flies")->name("flies");
+Route::view('/m/promo', "pages.promo")->name("promo");
+Route::view('/m/maintenance', "pages.maintenance")->name("maintenance");
+Route::view('/m/tour-search', "pages.tour-search")->name("tour-search");
+Route::view('/m/avia-search', "pages.avia-search")->name("avia-search");
