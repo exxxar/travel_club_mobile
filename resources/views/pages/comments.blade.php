3a708c03c6bc1b2cf2ea48cfd9b657d44c28e34f
unknown <exxxar@gmail.com>
2021-03-11 11:01:36 +0300

    add comments page

---
+++ resources/views/pages/comments.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.mobile')
+
+@section('content')
+    <preloader :time="3500"></preloader>
+
+    <header-block></header-block>
+
+    <script async src="https://comments.app/js/widget.js?3" data-comments-app-website="8DME3nOg" data-limit="5"></script>
+
+
+    <bottom-menu></bottom-menu>
+
+@endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
